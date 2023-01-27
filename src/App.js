755970a2eb55ba93c9c8ae64c686111e8d11d262
Patrick Costa <pl.costa@hotmail.com>
2023-01-27 14:31:28 -0300

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação' ,
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-end',
+      corPrimaria: '#82CFFA',
+      corSecundaria: 'EBF8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: 'A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: 'E06869',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: 'DB6EBF',
+      corSecundaria : '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: 'FFBA05',
+      corSecundaria: '#FFFSD9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: 'FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ] 
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
