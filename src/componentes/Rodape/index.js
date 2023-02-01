190eb11012eb1ddc46bdd80de45e3858f954eb31
Patrick Costa <pl.costa@hotmail.com>
2023-02-01 13:13:34 -0300

    Update 01_02_23

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,28 @@
+import './Rodape.css'
+
+const Rodape = () => {
+
+    return(
+        <footer className='rodape'>
+
+            
+            <img src="/imagens/fb.png" alt='facebook'/>
+            <img src="/imagens/tw.png" alt='twitter'/>
+            <img src="/imagens/ig.png" alt='instagram'/>
+            
+
+            
+            <img src='/imagens/logo.png' alt='organo'/>
+            
+
+            
+            <h3>Desenvolvido pela alura</h3>
+            
+
+        </footer>
+    )
+
+}
+
+export default Rodape
+
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,12 +5,13 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria };
 
     return(
-        <section className='time' style={css} >
+         (props.colaboradores.length > 0) ? <section className='time' style={css} >
             <h3 style={{ borderColor: props.corPrimaria }}>{props.name}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo}/>)}
             </div>
         </section>
+        : ''
     )
 }
 
