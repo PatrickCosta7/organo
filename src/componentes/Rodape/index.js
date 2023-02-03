6f57746d3fe3290be6aa8ff0c4993f743cc7c71f
Patrick Costa <pl.costa@hotmail.com>
2023-02-03 16:04:02 -0300

    Update

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,25 +3,35 @@ import './Rodape.css'
 const Rodape = () => {
 
     return(
-        
-        <section className='rodape'>
-            
-            <div className='elementos'>
-                <img src="/imagens/fb.png" alt='facebook'/>
-                <img src="/imagens/tw.png" alt='twitter'/>
-                <img src="/imagens/ig.png" alt='instagram'/>
-            </div>
-
-            <div className='elementos'>
-                <img src='/imagens/logo.png' alt='organo'/>
-            </div>
-
-            <div className='elementos'>
-                <h3>Desenvolvido pela alura</h3>
-            </div>
-        
-        </section>        
-
+    <footer className="footer">
+        <section>
+            <ul>
+                <li>
+                    <a href="facebook.com" target="_blank">
+                        <img src="/imagens/fb.png" alt='facebook'/>
+                    </a>
+                </li>
+                <li>
+                    <a href="twitter.com" target="_blank">
+                        <img src="/imagens/tw.png" alt='twitter'/>
+                    </a>
+                </li>
+                <li>
+                    <a href="instagram.com" target="_blank">
+                        <img src="/imagens/ig.png" alt='instagram'/>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Alura.
+            </p>
+        </section>
+    </footer>
     )
 
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -8,7 +8,7 @@ const Time = (props) => {
          (props.colaboradores.length > 0) ? <section className='time' style={css} >
             <h3 style={{ borderColor: props.corPrimaria }}>{props.name}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo}/>)}
+                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo}/>)}
             </div>
         </section>
         : ''
