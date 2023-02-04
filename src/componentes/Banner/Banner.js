b42dbced2003d96756025f33de8d200e88e7a8a4
Patrick Costa <pl.costa@hotmail.com>
2023-02-04 11:54:15 -0300

    Update

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,15 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 export const Banner = () => {
     //JSX
     return (
-        <header className='banner'>
-            <img src="/imagens/banner.png" alt="Imagem Banner"/>
-        </header>
+        <>{/* Elemento <Fragment> - essa sintax é mais simples - pratica comum de react - agrupa as tags children e retorna somente elas, simplificando */}
+            <header className='banner'>
+                <img src="/imagens/banner.png" alt="Imagem Banner"/>
+            </header>
+            {/* <h1>Teste</h1> */}
+        </>
     )
 }
 
